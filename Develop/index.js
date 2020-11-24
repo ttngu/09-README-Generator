d3ac7caa6a44cdffd6094ad576ea13b5d7c716b4
ttngu <tammyn137@gmail.com>
2020-11-23 18:32:24 -0800

    removed unneeded const

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 let generateMarkdown = require('./utils/generateMarkdown.js');
-const { createConnection } = require("net");
 
 
 // array of questions for user
